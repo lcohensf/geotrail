7850892c4d8840b3d723d5a59b5e0a93e8636b31
Lynn Cohen <lcohen@lcohen-ltm1.internal.salesforce.com>
2017-08-22 15:11:25 -0400

    fixed lc

--- force-app/main/default/aura/AccountList/AccountListController.js
+++ force-app/main/default/aura/AccountList/AccountListController.js
@@ -1,10 +1,10 @@
 ({
     doInit : function(component, event) {
-        console.log("in doInit");
+
         var action = component.get("c.findAll");
         action.setCallback(this, function(a) {
           component.set("v.accounts", a.getReturnValue()); 
-          console.log("got list of accounts back: " + a.getReturnValue);
+
         });
         $A.enqueueAction(action);
     }
